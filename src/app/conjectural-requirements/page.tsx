fcf6609d3a46324c72f13a84d5187632e7267bc6
Júlio Cesar Guimarães <jcguimaraes@gmail.com>
2026-03-05 15:06:20 -0300

    Revert "feat(canvas): add canvas area with resource streaming filling"
    This reverts commit c8e8d8ad30a7d23e8fd9db89eb1405a76d87ed20.

--- src/app/conjectural-requirements/page.tsx
+++ src/app/conjectural-requirements/page.tsx
@@ -10,9 +10,8 @@ import { useProject } from '@/contexts/ProjectContext';
 import { useRequirements } from '@/contexts/RequirementsContext';
 import { useSettings } from '@/contexts/SettingsContext';
 import { CopilotSidebar } from "@copilotkit/react-ui";
-import { useCopilotReadable, useLangGraphInterrupt, useCoAgent } from "@copilotkit/react-core";
+import { useCopilotReadable, useLangGraphInterrupt } from "@copilotkit/react-core";
 import { useAgent } from "@copilotkit/react-core/v2";
-import ResourceCanvas from '@/components/conjectural-requirements/ResourceCanvas';
 import StepProgress from '@/components/requirements/StepProgress';
 import InterruptForm from '@/components/requirements/InterruptForm';
 import Spinner from "@/components/ui/Spinner";
@@ -31,7 +30,6 @@ interface AgentState {
   require_brief_description: boolean;
   batch_mode: boolean;
   quantity_req_batch: number;
-  canvas_resources: string[];
   step1_elicitation: boolean;
   step2_analysis: boolean;
   step3_specification: boolean;
@@ -65,14 +63,6 @@ function CustomInput({ inProgress, onSend }: { inProgress: boolean; onSend: (tex
 }
 
 function ConjecturalRequirementsInner() {
-  const { state } = useCoAgent<AgentState>({
-    name: "sample_agent",
-    initialState: {
-      canvas_resources: [],
-    } as AgentState,
-  });
-
-  const canvasResources: string[] = state.canvas_resources || [];
 
   const { user } = useAuth();
   const { settings } = useSettings();
@@ -287,8 +277,6 @@ function ConjecturalRequirementsInner() {
         totalPages={totalPages}
         onPageChange={setCurrentPage}
       />
-
-      <ResourceCanvas resources={canvasResources} />
     </>
   );
 }
